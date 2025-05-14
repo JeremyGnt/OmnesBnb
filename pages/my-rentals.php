0bcd8f87001db8059d70f8ca9a37bdc200820ce2
paulf <paul.fievet@edu.ece.fr>
2025-05-14 22:45:46 +0200

    fonction activer ou non une location

--- pages/my-rentals.php
+++ pages/my-rentals.php
@@ -383,3 +383,26 @@
         </div>
     </div>
 </div>
+
+
+
+<!-- Modal de confirmation pour activer/désactiver une propriété -->
+<div class="modal fade" id="togglePropertyModal" tabindex="-1" aria-labelledby="togglePropertyModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="togglePropertyModalLabel">Confirmation</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p id="togglePropertyModalText">Êtes-vous sûr de vouloir modifier le statut de cette propriété ?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                <button type="button" class="btn btn-primary" id="confirmToggleProperty">Confirmer</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
